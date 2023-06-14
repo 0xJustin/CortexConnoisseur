af643db8dd7002e15787ca4d0e5f1ab52300cd1e
saloneeverma <72627410+saloneeverma@users.noreply.github.com>
2023-06-14 15:26:14 -0400

    fixed pubmed instead of pmc

--- cortexconnoisseur/utils.py
+++ cortexconnoisseur/utils.py
@@ -63,10 +63,16 @@ def get_text_from_springer(doi, save=True, api_key='a287f446500eaf7e1620969d0f09
     text = get_text_from_response(pdf_response, doi, 'springer', save)
     return text
 
-def get_text_from_pubmed(pmc_id, save=True):
+def get_text_from_pmc(pmc_id, save=True):
     pdf_url = f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_json/{pmc_id}/unicode"
     pdf_response = requests.get(pdf_url)
     text = get_text_from_response(pdf_response, pmc_id, 'pmc', save)
+    return text
+
+def get_text_from_pubmed(pubmed_id, save=True):
+    pdf_url = f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pubmed.cgi/BioC_json/{pubmed_id}/unicode"
+    pdf_response = requests.get(pdf_url)
+    text = get_text_from_response(pdf_response, pubmed_id, 'pubmed', save)
     return text
 
 def save_text_from_publisher_batch(id_list, publisher):
@@ -82,6 +88,7 @@ def save_text_from_publisher_batch(id_list, publisher):
         func = get_text_from_arxiv
     elif publisher == 'elsevier':
         func =  get_text_from_elsevier
+    
     
 
     with ThreadPoolExecutor() as executor:
